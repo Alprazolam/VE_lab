c79acf2a93a174f3bab2457a4b8713861e6a500a
Patrycja Dzialecka <patrycja.dzialecka.14@ucl.ac.uk>
2017-11-15 22:02:49 +0000

    Made collected objected children of the cart

---
+++ Assets/SupermarketInterior_v2/Scripts/DetectObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectObject : MonoBehaviour {
+	private Rigidbody cart;
+
+	// initialization
+	void Start ()
+	{
+		cart = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		other.transform.parent = cart.transform;
+	}
+}
